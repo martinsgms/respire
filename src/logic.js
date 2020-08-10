ef049af2616dea15de21d89dd64f86a39f784080
Gabriel Martins <gabriel.santos@egs.com.br>
2020-08-10 16:32:24 -0300

    commit inicial

---
+++ src/logic.js
@@ -0,0 +1,57 @@
+var $ = document.querySelector.bind(document);
+
+var circle = $('#circle');
+var mensagem = $('#mensagem');
+var ciclo = 1;
+var regressiva = 5; 
+
+const iniciarContagemRegressiva = () => {
+    contagem = setInterval(() => {
+
+        display(regressiva);
+        regressiva -= 1
+
+        if (regressiva < 0)
+            clearInterval(contagem)
+
+    }, 1000);
+}
+
+const inspira = () => {
+    trabalharAr('inspira');
+}
+
+const segura = () => {
+    trabalharAr('segura');
+}
+
+const expira = () => {
+    trabalharAr('expira');
+    concluirCiclo();
+}
+
+const trabalharAr = trabalho => {
+
+    display(trabalho);
+    
+    if (trabalho === 'segura')
+        return;
+
+    circle.classList.remove('inspira', 'expira');
+    circle.classList.add(trabalho);
+}
+
+const concluirCiclo = () =>{
+    ciclo++;
+
+    if (ciclo === 10)
+        encerrarSessao()
+} 
+
+const encerrarSessao = () => {
+    display('acabamos!');
+    clearInterval(sessao)
+}
+
+const display = text => mensagem.innerHTML = text;
+
---
+++ src/main.js
@@ -0,0 +1,16 @@
+
+iniciarContagemRegressiva();
+
+const sessao = setInterval(() => {
+    inspira();
+
+    setTimeout(() => {
+        segura();
+    }, 1500);
+
+    setTimeout(() => {
+        expira()
+    }, 4000);
+
+}, 7000);
+
